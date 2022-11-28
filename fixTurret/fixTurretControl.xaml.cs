72c83e4c52547cde5a187a6c1d08116d8801bf22
Александр Липкин <PuLs199@yandex.ru>
2022-11-28 22:55:08 +0300

    Добавьте файлы проекта.

---
+++ fixTurret/fixTurretControl.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace fixTurret
+{
+    public partial class fixTurretControl : UserControl
+    {
+
+        private fixTurret Plugin { get; }
+
+        private fixTurretControl()
+        {
+            InitializeComponent();
+        }
+
+        public fixTurretControl(fixTurret plugin) : this()
+        {
+            Plugin = plugin;
+            DataContext = plugin.Config;
+        }
+
+        private void SaveButton_OnClick(object sender, RoutedEventArgs e)
+        {
+            Plugin.Save();
+        }
+    }
+}
