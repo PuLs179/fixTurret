d58ef6cdc7f2f2f3b1f87871153f3487f96d7b44
Александр Липкин <PuLs199@yandex.ru>
2023-04-20 12:48:22 +0300

    add fix

--- fixTurret/fixTurret.cs
+++ fixTurret/fixTurret.cs
@@ -8,9 +8,21 @@ using Torch.API.Managers;
 using Torch.API.Plugins;
 using Torch.API.Session;
 using Torch.Session;
-
+using NLog;
+using Sandbox.Game.Weapons;
+using System;
+using System.Collections.Generic;
+using System.Reflection;
+using Torch.Managers.PatchManager;
+using Torch.Utils;
+using VRage.Game.ModAPI;
+using VRage.Game.Entity;
+using Vector2 = VRageMath.Vector2;
+using Vector3 = VRageMath.Vector3;
 namespace fixTurret
 {
+
+
     public class fixTurret : TorchPluginBase, IWpfPlugin
     {
 
@@ -23,6 +35,40 @@ namespace fixTurret
 
         private Persistent<fixTurretConfig> _config;
         public fixTurretConfig Config => _config?.Data;
+
+        [PatchShim]
+        public static class MyLargeTurretBasePatch
+        {
+
+            public static readonly Logger Log = LogManager.GetCurrentClassLogger();
+
+            internal static readonly MethodInfo update =
+                typeof(MyLargeTurretBase).GetMethod("UpdateAiWeapon", BindingFlags.Instance | BindingFlags.NonPublic) ??
+                throw new Exception("Failed to find patch method");
+
+            internal static readonly MethodInfo updatePatch =
+                typeof(MyLargeTurretBasePatch).GetMethod(nameof(UpdateAiWeaponPath), BindingFlags.Static | BindingFlags.Public) ??
+                throw new Exception("Failed to find patch method");
+
+            public static void UpdateAiWeaponPath(MyLargeTurretBase __instance)
+            {
+
+                if (__instance is MyLargeTurretBase && __instance.Target != null)
+                {
+                    __instance.SyncRotationAndOrientation();
+                    __instance.RotationAndElevation();
+
+                }
+
+
+            }
+
+            public static void Patch(PatchContext ctx)
+            {
+                ctx.GetPattern(update).Prefixes.Add(updatePatch);
+                Log.Info("Patching Successful MyLargeTurretBase!");
+            }
+        }
 
         public override void Init(ITorchBase torch)
         {
